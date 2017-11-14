f0753b5ab2faed21d1a8b7773b1cdf9b39c623e5
austinhannaleck <ahannaleck1@gmail.com>
2017-11-13 21:47:30 -0500

    Email script security features added

--- email.php
+++ email.php
@@ -1,32 +1,52 @@
 <?php
+
+// Only accept POST requests
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    // Remove any html tags and trim leading/trailing whitespace.
+    $name = strip_tags(trim($_POST['firstname'])) . " " . strip_tags(trim($_POST['lastname']));
+    $name = str_replace(array("\n", "\r"), array(" ", " "), $name);
     
-$fname = $_POST['firstname'];
-$lname = $_POST['lastname'];
-$email = $_POST['email'];
-$service = $_POST['services'];
-$message = $_POST['message'];
-$to = 'ahannaleck1@gmail.com';
-$subject = "The Cyber Architect - New Form Submission";
+    
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $service = $_POST['services'];
+    $message = strip_tags($_POST['message']);
+    
+    // If the email is invalid, send back an error code.
+    if(empty($message) || empty($service) || !filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        http_response_code(400);
+        echo "There was a problem with your submission, please try again.";
+        exit;
+    }
+    
+    // Set email recipient.
+    $to = 'ahannaleck1@gmail.com';
+    
+    // Set subject.
+    $subject = "$service request from $name";
+    
+    // Build message content.
+    $content  = "Name: $name\n";
+    $content .= "Email: $email\n";
+    $content .= "Message: $message\n";
 
-
-
-/* print_r($_POST); */
-
-/*
-echo "<br>" . $fname . " " . $lname . "<br>";
-echo $email . "<br>";
-echo $service . "<br>";
-echo $message;
-*/
-
-echo "Thank you for your submission, we will get back to you soon!";
-
-$msg = "$fname\n" .
-        "$lname\n" .
-        "$email\n" .
-        "$service\n" .
-        "$message";
-
-mail($to, $subject, $msg, 'From:' . $email);
+    if(true == mail($to, $subject, $content, 'From:' . $email))
+    {
+        http_response_code(200);
+        echo "Thank you for your submission, we will get back to you promptly!";
+    }
+    else
+    {
+        http_response_code(500);
+        echo "Something went wrong and we couldn't send your message. Please try again later.";
+    }
+}
+else
+{
+    // Not a POST request, send a forbidden error code (403)
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
 
 ?>
